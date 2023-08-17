83bfd376132b6e4fcb60e6fd8f267ac83915f4a4
Dustan Kraus <dustan.kraus@gmail.com>
2023-08-17 08:17:39 -0700

    Task 2

--- src/chatbot.h
+++ src/chatbot.h
@@ -25,14 +25,13 @@ public:
     // constructors / destructors
     ChatBot();                     // constructor WITHOUT memory allocation
     ChatBot(std::string filename); // constructor WITH memory allocation
-    ~ChatBot();
+    ~ChatBot(); // 1. Destructor
 
-    //// STUDENT CODE
-    // TODO: Insert code here
-    ////
-
-    ////
-    //// EOF STUDENT CODE
+    // Complete rule of 5
+    ChatBot(const ChatBot& source); // 2. Copy Constructor
+    ChatBot &operator=(const ChatBot& source); // 3. Copy Assignment Operator
+    ChatBot(ChatBot&& source); // 4. Move Constructor
+    ChatBot &operator=(ChatBot&& source); // 5. Move Assignment Operator
 
     // getters / setters
     void SetCurrentNode(GraphNode *node);
